58becd75e61c33b2c9e30e5d6a2216dca27f2cf2
Sachin Dhotre <150993688+ssd1999@users.noreply.github.com>
2023-11-24 15:51:44 +0530

    Add files via upload

---
+++ src/com/cloud/RomanToInteger.java
@@ -0,0 +1,45 @@
+package com.cloud;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a Roman numeral: ");
+        String romanInput = scanner.nextLine().toUpperCase();
+
+        int integerValue = romanToInteger(romanInput);
+        
+        System.out.println("The integer value of " + romanInput + " is " + integerValue);
+    }
+
+    public static int romanToInteger(String s) {
+        HashMap<Character, Integer> romanNumerals = new HashMap<>();
+        romanNumerals.put('I', 1);
+        romanNumerals.put('V', 5);
+        romanNumerals.put('X', 10);
+        romanNumerals.put('L', 50);
+        romanNumerals.put('C', 100);
+        romanNumerals.put('D', 500);
+        romanNumerals.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char numeral = s.charAt(i);
+            int value = romanNumerals.get(numeral);
+
+            if (value < prevValue) {
+                result -= value;
+            } else {
+                result += value;
+            }
+
+            prevValue = value;
+        }
+
+        return result;
+    }
+}
